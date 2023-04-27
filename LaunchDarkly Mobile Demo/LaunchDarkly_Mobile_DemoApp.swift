529a5fc4b6232892359b9aa1d6e70953d3f0af72
Alexander Hardman <alexander.hardman@gmail.com>
2023-04-27 10:04:01 -0400

    Initial Commit

---
+++ LaunchDarkly Mobile Demo/LaunchDarkly_Mobile_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchDarkly_Mobile_DemoApp.swift
+//  LaunchDarkly Mobile Demo
+//
+//  Created by Alex Hardman on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct LaunchDarkly_Mobile_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
